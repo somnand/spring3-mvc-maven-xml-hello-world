9a464909d37bd6643299588b98fdc9a0f4c2ebb1
Somsurya Nanda <somsuryananda@gmail.com>
2016-08-14 13:48:48 +0530

    Added request mapping of "/welcome" in HelloController.java

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 //@RequestMapping("/hello")
@@ -12,7 +13,16 @@ public class HelloController {
 	@RequestMapping(value="/index",method=RequestMethod.GET)
 	public String index(ModelMap model)
 	{
-		model.addAttribute("modelMessage","Hello World ! - from Spring MVC");
+		model.addAttribute("modelMessage","index: Hello World ! - from Spring MVC");
 		return "hello";		
-	}	
+	}
+	
+	@RequestMapping("/welcome")
+	public ModelAndView welcomeMethod()
+	{
+		ModelAndView model=new ModelAndView();
+		model.addObject("modelMessage","welcome: Hello World ! - from Spring MVC");
+		
+		return model;
+	}
 }
